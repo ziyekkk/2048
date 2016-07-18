7ecc31735ddc497eae10fcc5533b9e575034457f
ziyekkk <1024465429@qq.com>
2016-07-18 20:59:02 +0800

    第一个版本
    版本1

---
+++ 2048js/animation.js
@@ -0,0 +1,10 @@
+function showMoveAnimation(fromx,fromy,tox,toy){
+	var numberCell=$("#number-cell-"+fromx+"-"+fromy);
+	numberCell.animate({
+		top:getPosTop(tox,toy),
+		left:getPosLeft(tox,toy)
+	},200);
+}
+function updateScore(score){
+	$("#score").text(score);
+}
